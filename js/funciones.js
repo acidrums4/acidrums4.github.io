172916aaebf650f8a4d2b7f601cee57f8df85d78
acidrums4 <acidrums4@gmail.com>
2017-01-05 13:43:02 -0500

    Imágenes adaptables.

--- js/funciones.js
+++ js/funciones.js
@@ -120,7 +120,7 @@ jQuery(document).ready(function(event){
     $('rect.clip').attr('height',0);
 
     function progressComplete(){
-      percentage = 100;
+      /*percentage = 100;
       $('svg.ip-inner').removeClass('loading');
       container.removeClass('unload loading').addClass('loaded');
 
@@ -129,7 +129,7 @@ jQuery(document).ready(function(event){
         window.removeEventListener( 'scroll', noscroll );
         isAnimating = false;
 				$(this).off(e);
-      });
+      });*/
     }
 
     function countImages(){

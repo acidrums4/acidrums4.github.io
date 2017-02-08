c5e55cb381fdc2662eacea49abb60ec3dfdeba9f
acidrums4 <acidrums4@gmail.com>
2017-02-07 19:53:36 -0500

    Intentar arreglar animación de 'Acerca de'.

--- js/funciones.js
+++ js/funciones.js
@@ -354,8 +354,8 @@ jQuery(document).ready(function(event){
 		
 		//if (('beginElement' in eyeL) && ('beginElement' in eyeR)){
       //console.log("Sirva, carajo!");
-			var animateL = document.createElementNS("http://www.w3.org/2000/svg","animate");
-			var animateR = document.createElementNS("http://www.w3.org/2000/svg","animate");
+			var animateL = document.createElementNS("http://www.w3.org/2000/svg","animateMotion");
+			var animateR = document.createElementNS("http://www.w3.org/2000/svg","animateMotion");
 			animateL.setAttribute('attributeName','d');
 			animateR.setAttribute('attributeName','d');
 			animateL.setAttribute('repeatCount','indefinite');
